65fc27710953ac84ab0e3391c4d2f4cc83db0817
Blake Patenaude <blakepat@me.com>
2022-01-21 23:07:28 -0800

    Initial Commit

---
+++ STOKGN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  STOKGN
+//
+//  Created by Blake Patenaude on 2022-01-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ STOKGN/STOKGNApp.swift
@@ -0,0 +1,17 @@
+//
+//  STOKGNApp.swift
+//  STOKGN
+//
+//  Created by Blake Patenaude on 2022-01-21.
+//
+
+import SwiftUI
+
+@main
+struct STOKGNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
